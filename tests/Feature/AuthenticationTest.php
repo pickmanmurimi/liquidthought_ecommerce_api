296c909629d0f3f39e0078f0705a43a3d209f5a1
pickman murimi <pickmanmurimi@gmail.com>
2021-10-29 19:46:24 +0300

    first commit

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+    /**
+     * Tests if a user can log in
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
