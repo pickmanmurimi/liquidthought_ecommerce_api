d06d9ccc67064eda7f276f3a251670d268258218
pickman murimi <pickmanmurimi@gmail.com>
2021-10-30 00:54:52 +0300

    added items table

---
+++ database/migrations/2021_10_29_215046_create_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+
+            $table->float('unit_price');
+            $table->string('sku');
+            $table->string('image_url');
+            $table->boolean('isAvailable');
+            $table->text('description');
+            $table->string('currency');
+            $table->string('name');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
