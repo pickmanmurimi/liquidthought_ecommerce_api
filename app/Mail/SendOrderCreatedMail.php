cb2934ad8bb128f17f4d37071a5bcfdc5ec06573
pickman murimi <pickmanmurimi@gmail.com>
2021-11-25 14:44:44 +0300

    Run SendOrderCreatedMail in a queue job.

--- app/Mail/SendOrderCreatedMail.php
+++ app/Mail/SendOrderCreatedMail.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class SendOrderCreatedMail extends Mailable
+class SendOrderCreatedMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
